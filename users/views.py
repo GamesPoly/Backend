47b723e22b9e694729508b2ae221fb9bb72489df
RuslanitoSS <145444349+RuslanitoSS@users.noreply.github.com>
2025-04-24 17:56:44 +0300

    Update views.py 2

--- users/views.py
+++ users/views.py
@@ -10,7 +10,7 @@ class RegisterView(APIView):
     def post(self, request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            user = serializer.save()  
             token, created = Token.objects.get_or_create(user=user)
             return Response({'token': token.key}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
