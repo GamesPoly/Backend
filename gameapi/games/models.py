577c6f35dd7c85f219f6779c2c7cf327f44c7659
weefce <valeri05.lera@mail.ru>
2025-05-18 14:26:22 +0300

    Добавлены поля в модель Game для соответствия форме загрузки игр

--- gameapi/games/models.py
+++ gameapi/games/models.py
@@ -1,10 +1,49 @@
 from django.db import models
 
+class Platform(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+PLAYER_MODES = [
+    ('single', 'Для одного игрока'),
+    ('multi', 'Мультиплеер'),
+]
+
+
 class Game(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    photo_url = models.URLField(blank=True, null=True)  # Change from ImageField to URLField
-    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=46, null=True, blank=True)
+    description = models.CharField(max_length=1000, null=True, blank=True)
+    short_description = models.CharField(max_length=300, null=True, blank=True)
+    keywords = models.CharField(max_length=255, null=True, blank=True)
+    tags = models.CharField(max_length=255, null=True, blank=True)
+
+    platforms = models.ManyToManyField(Platform)
+
+    cpu = models.CharField(max_length=255, null=True, blank=True)
+    ram = models.CharField(max_length=255, null=True, blank=True)
+    gpu = models.CharField(max_length=255, null=True, blank=True)
+    disk = models.CharField(max_length=255, null=True, blank=True)
+    os = models.CharField(max_length=255, null=True, blank=True)
+    extra_reqs = models.TextField(blank=True)
+
+    developer_name = models.CharField(max_length=255, null=True, blank=True)
+    contact_email = models.EmailField(null=True, blank=True)
+
+    genres = models.ManyToManyField(Genre)
+    main_genre = models.CharField(max_length=255, null=True, blank=True)
+
+    player_mode = models.CharField(max_length=10, choices=PLAYER_MODES, null=True, blank=True)
+    awards = models.TextField(blank=True)
 
     def __str__(self):
         return self.title
